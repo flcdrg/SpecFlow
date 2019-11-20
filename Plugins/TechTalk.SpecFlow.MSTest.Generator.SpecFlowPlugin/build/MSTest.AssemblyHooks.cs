b74c379d61828b30e31ae129f9dcfcd1d5a2a803
David Perfors <dnperfors@gmail.com>
2019-11-20 13:10:49 +0100

    Added GenerateCodeAttribute to AssemblyHooks in order to disable code analysis.

--- Plugins/TechTalk.SpecFlow.MSTest.Generator.SpecFlowPlugin/build/MSTest.AssemblyHooks.cs
+++ Plugins/TechTalk.SpecFlow.MSTest.Generator.SpecFlowPlugin/build/MSTest.AssemblyHooks.cs
@@ -1,7 +1,9 @@
 ﻿using System.Diagnostics;
+using System.CodeDom.Compiler;
 using global::Microsoft.VisualStudio.TestTools.UnitTesting;
 using global::TechTalk.SpecFlow;
 
+[GeneratedCode]
 [TestClass]
 public class PROJECT_ROOT_NAMESPACE_MSTestAssemblyHooks
 {
